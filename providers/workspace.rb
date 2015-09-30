da475f017f9fd7e14efcc3a019c1d91b9724bb3f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-09-30 10:21:05 -0300

    document the workspace::buildconf attribute

--- providers/workspace.rb
+++ providers/workspace.rb
@@ -43,9 +43,9 @@ def bootstrap(resource)
     vcs_type = buildconf.delete('type')
     vcs_url  = buildconf.delete('url')
     vcs_options = buildconf.map { |k, v| "'#{k}=#{v}'" }
-    script "autoproj bootstrap" do
+    execute "autoproj bootstrap" do
         environment 'AUTOPROJ_BOOTSTRAP_IGNORE_NONEMPTY_DIR' => '1', 'AUTOPROJ_OSDEPS_MODE' => 'all'
-        code "#{resource.ruby} '#{bootstrap}' '#{vcs_type}' '#{vcs_url}' #{vcs_options.join(" ")}"
+        command "#{resource.ruby} '#{bootstrap}' '#{vcs_type}' '#{vcs_url}' #{vcs_options.join(" ")}"
         cwd resource.dir
         user resource.user
     end
--- resources/workspace.rb
+++ resources/workspace.rb
@@ -1,6 +1,10 @@
 actions :bootstrap, :update, :build
 default_action :build
 
+# The definition of the build configuration for the bootstrap
+#
+# 'type' and 'url' are the first two argument to boostrap and the rest are
+# passed as key=value
 attribute :buildconf, kind_of: Hash,
     required: true
 
@@ -11,6 +15,8 @@ attribute :user, kind_of: String,
     required: true
 attribute :dir, name_attribute: true, kind_of: String,
     required: true
+attribute :cache_dir, kind_of: String,
+    required: false
 attribute :update, default: false
 attribute :seed_config, kind_of: String
 
